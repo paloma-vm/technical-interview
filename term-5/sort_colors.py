4daf231cef0333d17c246d59841303f26b7b638e
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-07-12 00:13:15 -0600

    Update sort_colors.py

--- term-5/sort_colors.py
+++ term-5/sort_colors.py
@@ -38,7 +38,50 @@
 
 # ------ SOLUTION ----------------
 # can't use sort
-# create a pointer
+# create a pointer, or pointers
 # compare the pointer to the first element,
 # if the pointer is greater or equal to the element it is being compared to,
 # move the pointer up an index space 
+
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # make pointers
+        low = 0
+        middle = 0
+        high = len(nums) - 1 # the last element
+
+        # do as long as the middle pointer is less than or equal to the high pointer
+        while middle <= high:
+            # if nums at the index of the middle pointer is zero,
+            if nums[middle] == 0:
+                # switch it with nums[low] and increment low and middle pointers
+                nums[middle], nums[low] = nums[low], nums[middle]
+                low += 1
+                middle += 1
+            # if nums[middle] is 1, increment middle pointer
+            elif nums[middle] == 1:
+                middle += 1
+            # lastly, if nums[middle] is 2, switch  nums of middle and high and decrement high
+            else:
+                nums[middle], nums[high] = nums[high], nums[middle]
+                high -= 1
+            # don't return anything!
+
+# Time complexity:
+    # worst: O(n)
+
+# Notes:
+    # this is also known as the Dutch Flag Algortithm (red, white, blue) 
+    # or 3-way quicksort
+    
+    
+
+
+
+
+  
